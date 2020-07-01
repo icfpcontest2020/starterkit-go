d3873ec80492310ff068e9cc3c51c59584112013
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2020-07-01 19:05:18 +0900

    Send a sample HTTP request.
    The code does roughly the same thing as starterkit-csharp.
    https://github.com/icfpcontest2020/starterkit-csharp/blob/master/app/Program.cs

--- app/main.go
+++ app/main.go
@@ -2,9 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
-	fmt.Printf("%s %s\n", os.Args[1], os.Args[2])
+	serverURL := os.Args[1]
+	playerKey := os.Args[2]
+
+	fmt.Printf("ServerUrl: %s; PlayerKey: %s\n", serverURL, playerKey)
+
+	res, err := http.Get(fmt.Sprintf("%s?playerKey=%s", serverURL, playerKey))
+	if err != nil {
+		log.Fatalf("Failed: %v", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		log.Fatalf("Failed: got status %d, want %d", res.StatusCode, http.StatusOK)
+	}
 }
